4c06b80322055056467fd5244fde58b4b339cb5e
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-02-19 23:18:31 +0900

    Middleware Added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT =4000;
 
 const app = express();
 
+const logger = (req, resp, next) =>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const handleHome = (req,resp) => {
+    return resp.end();
+};
+
+app.get("/", logger, handleHome);
+
 const handleListening = () => console.log(`🚦 Server listening on http://localhost:${PORT} 😃`);
 
 app.listen(PORT, handleListening);
