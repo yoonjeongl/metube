deb41bd377da9e4e228c587e0b376bc28482727c
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-02-19 22:28:55 +0900

    express server launching no app

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT =4000;
+
+const app = express();
+
+const handleListening = () => console.log(`🚦 Server listening on http://localhost:${PORT} 😃`);
+
+app.listen(PORT, handleListening);
