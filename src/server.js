157cf51f1582cf6d1a49494c8f9d14ed635bd6d7
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-02-20 22:53:27 +0900

    External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+import morgan from "morgan"; // MiddleWare. Logger.
 
-const PORT =4000;
+const PORT = 4000;
 
 const app = express();
-
-const logger = (req, resp, next) =>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+const logger = morgan("dev"); // more options like common, combined, etc
 
 const handleHome = (req,resp) => {
-    return resp.end();
+    return resp.send("<H1>This is home</H1>");
 };
 
-app.get("/", logger, handleHome);
+const handleLogin = (req, resp) => {
+    return resp.send("<H1>Please enter your ID/PW</H1>")
+}
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`🚦 Server listening on http://localhost:${PORT} 😃`);
 
