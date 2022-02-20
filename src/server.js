7ff8979a60b28acd35fecbd21077c67379360bd4
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-02-21 00:53:50 +0900

    cleanRouterCodes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan"; // MiddleWare. Logger.
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -7,20 +10,8 @@ const app = express();
 const logger = morgan("dev"); // more options like common, combined, etc
 app.use(logger);
 
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video")
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
-app.use("videos", videoRouter);
+app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => console.log(`🚦 Server listening on http://localhost:${PORT} 😃`);
