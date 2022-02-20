4bf71d60dd7d1de2e738509ec0895239c7b7982c
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-02-20 23:49:21 +0900

    Router Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Metube !!
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delelte -> Delete a comment of a video
--- src/server.js
+++ src/server.js
@@ -5,18 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev"); // more options like common, combined, etc
-
-const handleHome = (req,resp) => {
-    return resp.send("<H1>This is home</H1>");
-};
-
-const handleLogin = (req, resp) => {
-    return resp.send("<H1>Please enter your ID/PW</H1>")
-}
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video")
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`🚦 Server listening on http://localhost:${PORT} 😃`);
 
