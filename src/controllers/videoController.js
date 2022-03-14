9426583a192b4a44d8eae5bd987598661803322f
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-14 18:05:38 +0900

    #5.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Various Videos")
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("Edit");
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/server.js
+++ src/server.js
@@ -8,8 +8,10 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev"); // more options like common, combined, etc
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
