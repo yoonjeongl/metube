ecfadb9741e52d6531da51b460e10b1b4328917a
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-14 18:27:34 +0900

    #5.3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("Edit");
+export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
