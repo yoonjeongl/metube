c1d2a748d0b0604c1659045c7511cfad311b3019
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-16 21:46:46 +0900

    #5.8

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,7 @@
-const fakeUser = {
-    username: "Yoong",
-    loggedIn: true,
-}
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    res.render("home", {pageTitle: "Home", videos});
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 
