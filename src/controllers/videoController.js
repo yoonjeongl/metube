ab823f23e9e60361c2ff556a2d712f7fd1c21d5e
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-17 22:14:23 +0900

    #6.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,40 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 mins ago",
+        views:59,
+        id:1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 mins ago",
+        views:59,
+        id:2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 mins ago",
+        views:59,
+        id:3,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 mins ago",
-            views:59,
-            id:1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 mins ago",
-            views:59,
-            id:1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 mins ago",
-            views:59,
-            id:1,
-        },
-    ];
     res.render("home", {pageTitle: "Home", videos});
 };
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
 
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render("watch", {pageTitle: `Watching ${video.title}`});
+};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("deleteVideo");
-};
+export const deleteVideo = (req, res) => res.send("deleteVideo");
