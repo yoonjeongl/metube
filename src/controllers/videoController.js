87fc87e736dbf84571a72f1a05a99cda88c685ab
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-17 22:52:48 +0900

    #6.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,7 @@ let videos = [
         rating: 5,
         comments: 2,
         createdAt: "2 mins ago",
-        views:59,
+        views:1,
         id:2,
     },
     {
@@ -20,7 +20,7 @@ let videos = [
         rating: 5,
         comments: 2,
         createdAt: "2 mins ago",
-        views:59,
+        views:92,
         id:3,
     },
 ];
@@ -29,10 +29,10 @@ export const trending = (req, res) => {
     res.render("home", {pageTitle: "Home", videos});
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
-    res.render("watch", {pageTitle: `Watching ${video.title}`});
+    res.render("watch", {pageTitle: `Watching ${video.title}`, video});
 };
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
