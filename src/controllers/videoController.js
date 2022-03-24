0d04269dd16cf0729563e539502ac15861f68ae4
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-24 15:36:33 +0900

    #6.21

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,21 +33,35 @@ export const getEdit = async (req, res) => {
     return res.render("edit", {pageTitle: `Edit ${video.title}`, video});
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({_id: id});
+    if(!video)
+    {    
+        return res.render("404", {pageTitle: "Video is not found"});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title, description, hashtags:hashtags.split(",").map((word => word.startsWith("#") ? word : `#${word}`)),
+    });
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((word => word.startsWith("#") ? word : `#${word}`));
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: `Upload Video`});
 };
+
 export const postUpload = async(req, res) => {
     const { title, description, hashtags } = req.body;
     try{
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags: hashtags.split(",").map((word => word.startsWith("#") ? word : `#${word}`)),
         });
         return res.redirect("/");
     }catch (error){
