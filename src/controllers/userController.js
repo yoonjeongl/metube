642ce36419f2f5a6465b06370657fa7855be2a61
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-24 22:35:23 +0900

    #7.0

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
-export const join = (req, res) => res.send("Join Us!");
+export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+export const postJoin = (req, res) => {};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
