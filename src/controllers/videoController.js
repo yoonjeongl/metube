5a7045b91fd38545351e2e677e7a5b072b2b25f9
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-24 16:37:28 +0900

    #6.25

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -64,3 +64,9 @@ export const postUpload = async(req, res) => {
         return res.render("upload", {pageTitle: "Upload Video", errorMessage:error._message,});
     };
 }
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-z]{24})", watch);
 videoRouter.route("/:id([0-9a-z]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-z]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
