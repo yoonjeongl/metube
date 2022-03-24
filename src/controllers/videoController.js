a1bc1ce5546e299ffeabb12e9e96606c3f92757e
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-24 16:24:25 +0900

    #6.24

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,18 +36,14 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
-    const video = await Video.exit({_id: id});
+    const video = await Video.exists({_id: id});
     if(!video)
     {    
         return res.render("404", {pageTitle: "Video is not found"});
     }
     await Video.findByIdAndUpdate(id, {
-        title, description, hashtags:hashtags.split(",").map((word => word.startsWith("#") ? word : `#${word}`)),
+        title, description, hashtags: Video.formatHashtags(hashtags),
     });
-    video.title = title;
-    video.description = description;
-    video.hashtags = hashtags.split(",").map((word => word.startsWith("#") ? word : `#${word}`));
-    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
@@ -61,7 +57,7 @@ export const postUpload = async(req, res) => {
         await Video.create({
             title,
             description,
-            hashtags,
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     }catch (error){
