17d96fe0aee10ad58241056f09b10a2f7bf0bb17
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-24 17:21:44 +0900

    #6.27

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -72,9 +72,15 @@ export const deleteVideo = async (req, res) => {
 }
 
 export const search = async (req, res) =>{
-    const {keyword} = req.query;
+    const { keyword } = req.query;
+    let videos = [];
     if(keyword){
         // search
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(`${keyword}$`, "i"),
+            }
+        });
     }
-    return res.render("search", {pageTitle: "Search"});
-}
+    return res.render("search", {pageTitle: "Search", videos});
+} 
