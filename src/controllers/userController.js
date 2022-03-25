3e58535c830bff2882dcfb8a076f041f89cdc10e
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-25 16:13:13 +0900

    #7.3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,9 +2,22 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
 export const postJoin = async (req, res) => {
-    const {name, username, email, password, location} = req.body;
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "Join";
+    const exists = await User.exists({ $or: [{ username }, {email}] });
+    if (exists){
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This username/email is already taken.",
+        });
+    }
+    if (password !== password2)
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+    });        
     await User.create({
-        name, username, email, password, location,
+        name, username, email, password, password2, location,
     });
     return res.redirect("/login");
 };
