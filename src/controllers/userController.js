c61a85fd6f8b310e279a23fb10de5a3ca0932d58
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-25 17:55:55 +0900

    #7.6

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
 export const postJoin = async (req, res) => {
@@ -33,15 +34,24 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
 export const postLogin = async (req, res) => {
     const {username, password} = req.body;
-    const exists = await User.exists({ username });
-    if( !exists ){
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    console.log(user.password);
+    if( !user ){
         return res.status(400).render("login", {
-            pageTitle: "Login", errorMessage: "An account with this username does not exist."
+            pageTitle, 
+            errorMessage: "An account with this username does not exist."
         });
     }
-
-    res.end();
-}
+    const ok = await bcrypt.compare(password, user.password);
+    if ( !ok ){
+        return res.status(400).render("login", {
+            pageTitle, 
+            errorMessage: "Wrong password"
+        });
+    }
+    return res.redirect("/");
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
