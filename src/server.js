73481d5327bdde9f2292f52d1cd9a6a465943d6c
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-25 18:13:51 +0900

    #7.7

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan"; // MiddleWare. Logger.
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev"); // more options like common, combined, etc
@@ -11,6 +12,14 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+    })
+);
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
