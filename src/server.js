19636620ac45cf16758ec881fa9ee24999c515b6
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-29 20:53:23 +0900

    #7.12

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan"; // MiddleWare. Logger.
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -18,6 +19,7 @@ app.use(session({
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/metube" }),
     })
 );
 
