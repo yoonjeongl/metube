5241421d9ca5aa3736763f4400a236426e5588e3
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-03-29 21:20:23 +0900

    #7.14

--- src/server.js
+++ src/server.js
@@ -16,10 +16,13 @@ app.use(logger);
 app.use(express.urlencoded({extended: true}));
 
 app.use(session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/metube" }),
+//    cookie: {
+//        maxAge: 20000,
+//    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
     })
 );
 
