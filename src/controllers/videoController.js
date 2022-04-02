fbcd85d6a896933c440c4a316cfe15987c7fd2aa
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-03 00:21:22 +0900

    #8.12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,14 +14,12 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
-    const owner = await User.findById(video.owner);
-//    if(video === null)
+    const video = await Video.findById(id).populate("owner");
     if(!video)
     {    
         return res.status(404).render("404", {pageTitle: "Video is not found"});
     }
-    return res.render("watch", {pageTitle: video.title , video, owner });
+    return res.render("watch", {pageTitle: video.title , video });
 };
 
 export const getEdit = async (req, res) => {
