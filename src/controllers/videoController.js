1facf9395d66315ec519c20589f029667026f24a
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-03 00:03:39 +0900

    #8.11

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 import Video from "../models/Video";
-
+import User from "../models/User";
 /*vVideo.find({}, (error, videos) => {
     if(error){
         return res.render("server-error");
@@ -15,12 +15,13 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    const owner = await User.findById(video.owner);
 //    if(video === null)
     if(!video)
     {    
         return res.status(404).render("404", {pageTitle: "Video is not found"});
     }
-    return res.render("watch", {pageTitle: video.title , video});
+    return res.render("watch", {pageTitle: video.title , video, owner });
 };
 
 export const getEdit = async (req, res) => {
@@ -52,6 +53,7 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async(req, res) => {
+    const { user: {_id} } = req.session;
     const { path: fileUrl } = req.file;
     const { title, description, hashtags } = req.body;
     try{
@@ -59,6 +61,7 @@ export const postUpload = async(req, res) => {
             title,
             description,
             fileUrl,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
