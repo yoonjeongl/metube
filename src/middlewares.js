cb543756b54a5dd96445ac983fdb3c3ad8c1739b
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-02 23:21:30 +0900

    #8.9

--- src/middlewares.js
+++ src/middlewares.js
@@ -22,6 +22,16 @@ export const publicOnlyMiddleware = (req, res, next) => {
 }
 
 const multer = require('multer');
-export const uploadFiles = multer({
-    dest: "uploads/"
+
+export const avatarUpload = multer({ 
+    dest: "uploads/avatars/", 
+    limits: {
+        fileSize: 3000000,
+    },
+});
+export const videoUpload = multer({
+    dest: "uploads/videos/", 
+    limits: {
+        fileSize: 10000000,
+    },
 });
