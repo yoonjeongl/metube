9aaa057143100d8e1ef409b51306131e2d1d94e5
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-03 00:36:18 +0900

    #8.13

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -55,13 +55,16 @@ export const postUpload = async(req, res) => {
     const { path: fileUrl } = req.file;
     const { title, description, hashtags } = req.body;
     try{
-        await Video.create({
+        const newVideo = await Video.create({
             title,
             description,
             fileUrl,
             owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
+        const user = await User.findById(_id);
+        user.videos.push(newVideo);
+        user.save();
         return res.redirect("/");
     }catch (error){
         return res.status(400).render("upload", {pageTitle: "Upload Video", errorMessage:error._message,});
