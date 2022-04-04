cb2a0e9341ef029256806cd9e9e3880b13a94a48
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-04 19:38:09 +0900

    #11.2

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play")
 const muteBtn = document.getElementById("mute")
 const time = document.getElementById("time")
-const volume = document.getElementById("volume")
+const volumeRange = document.getElementById("volume")
 
 const handlePlayClick = (e) => {
     // if the video is playing, pause it
@@ -11,12 +11,19 @@ const handlePlayClick = (e) => {
     } else{
         video.pause();
     }
+    playBtn.innerText.paused ? "Play" : "Pause";
 };
 
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
 const handleMute = (e) => {
-
+    if (video.muted){
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
