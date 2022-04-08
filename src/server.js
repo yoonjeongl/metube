20e5019677a33b5092305762c3630c87d37109fa
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-08 21:57:58 +0900

    #14.1

--- src/server.js
+++ src/server.js
@@ -27,6 +27,11 @@ app.use(session({
     })
 );
 
+app.use((req,res,next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");  
+    next();  
+});
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
