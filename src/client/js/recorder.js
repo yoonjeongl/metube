944a921e395fe76146c8561dbdcc9c48a530d01b
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-09 19:37:18 +0900

    #14.6

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -46,7 +46,7 @@ const handleDownload = async() => {
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
     downloadFile(mp4Url, "MyRecording.mp4");
-    downloadFile(mp4Url, "MyThumbnail.mp4");
+    downloadFile(mp4Url, "MyThumbnail.jpg");
 
     ffmpeg.FS("unlink", files.input);
     ffmpeg.FS("unlink", files.output);
@@ -60,19 +60,11 @@ const handleDownload = async() => {
     actionBtn.addEventListener("click", handleStart);
 };
 
-const handleStop = () =>
-{
-    actionBtn.innerText = "Download Recording";
-    actionBtn.removeEventListener("click", handleStop);
-    actionBtn.addEventListener("click", handleDownload);
-    recorder.stop();
-};
-
 const handleStart = () =>
 {
-    actionBtn.innerText = "Stop Recording";
+    actionBtn.innerText = "Recording";
+    actionBtn.disabled = true;
     actionBtn.removeEventListener("click", handleStart);
-    actionBtn.addEventListener("click", handleStop);
     recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
         videoFile = URL.createObjectURL(event.data);
@@ -80,18 +72,27 @@ const handleStart = () =>
         video.src = videoFile;
         video.loop = true;
         video.play();
+        actionBtn.innerText = "Download";
+        actionBtn.disabled = false;
+        actionBtn.addEventListener("click", handleDownload);
     };
     recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 5000);
 };
 
-const init = async() => {
+const init = async () => {
     stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true,
+      audio: false,
+      video: {
+        width: 1024,
+        height: 576,
+      },
     });
     video.srcObject = stream;
     video.play();
-};
+}
 
 init();
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,6 +7,7 @@ const videoRouter = express.Router();
 videoRouter.get("/:id([0-9a-z]{24})", watch);
 videoRouter.route("/:id([0-9a-z]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 videoRouter.route("/:id([0-9a-z]{24})/delete").all(protectorMiddleware).get(deleteVideo);
-videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.single("video"), postUpload);
+videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.fields([
+    {name:"video"}, {name:"thumb"}]), postUpload);
 
 export default videoRouter;
