c3130d48a641fdb0720e1b4b93fae169fd27d1e4
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-15 20:11:42 +0900

    server.js cors

--- src/server.js
+++ src/server.js
@@ -31,7 +31,8 @@ app.use(session({
 
 app.use((req,res,next) => {
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");  
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    res.header("Cross-Origin-Resource-Policy", "cross-origin");
     next();  
 });
 app.use(flash());
