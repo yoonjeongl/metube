fa0f917f1303d611e5c71c4814e32a20a6db2cc3
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-15 20:53:36 +0900

    server.js cors2

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(session({
 );
 
 app.use((req,res,next) => {
-    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Embedder-Policy", "credentialless");
     res.header("Cross-Origin-Opener-Policy", "same-origin");
     res.header("Cross-Origin-Resource-Policy", "cross-origin");
     next();  
