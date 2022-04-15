6dce60cf749e0d26a80ff24a813aea7e09e6383a
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-15 21:01:15 +0900

    server.js playerTimer

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -116,7 +116,7 @@ const handleEnded = () => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("canplay", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
