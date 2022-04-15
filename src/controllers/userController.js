74bd7269087df5693ef670894edc8011f7a2fba8
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-15 14:31:06 +0900

    s3 upload

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,7 +147,7 @@ export const postEdit = async (req, res) => {
     } = req;
     const updatedUser = await User.findByIdAndUpdate(
         _id, {
-            avatarUrl: file ? file.path : avatarUrl,
+            avatarUrl: file ? file.location : avatarUrl,
             name, 
             email, 
             username, 
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,23 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+    },
+})
+
+const multerUploader = multerS3({
+    s3: s3,
+    bucket: "metubee",
+    acl: "public-read",
+})
+
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.siteName = "Metube";
+    res.locals.siteName = "wowtubee";
     res.locals.loggedInUser = req.session.user || {};
     next();
 }
@@ -30,10 +47,12 @@ export const avatarUpload = multer({
     limits: {
         fileSize: 3000000,
     },
+    storage: multerUploader,
 });
 export const videoUpload = multer({
     dest: "uploads/videos/", 
     limits: {
         fileSize: 100000000,
     },
+    storage: multerUploader,
 });
