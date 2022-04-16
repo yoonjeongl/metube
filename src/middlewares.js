ba86f5688dcc0bf9307def17744d3605ea6df068
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2022-04-16 17:05:03 +0900

    video uploader auto content type

--- src/middlewares.js
+++ src/middlewares.js
@@ -20,6 +20,7 @@ const s3VideoUploader = multerS3({
     s3: s3,
     bucket: "wowtubee/videos",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
